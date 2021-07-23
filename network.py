9aefb81c31a9fbce3db22389e62d05445c3bc6c9
Desktop Casa Arg <hbermudez.info@gmail.com>
2021-07-23 12:46:44 -0300

    Limpiando codigo y estableciendo conexion con los clientes

--- network.py
+++ network.py
@@ -1,4 +1,5 @@
 import socket
+import pickle
 
 class Network:
     def __init__(self):
@@ -6,24 +7,21 @@ class Network:
         self.server = "192.168.0.242"
         self.port = 5555
         self.addr = (self.server, self.port)
-        self. id = self.connect()
-        print(self.id)
+        self.p = self.connect()
+
+    def get_p(self):
+        return self.p
 
     def connect(self):
         try:
             self.client.connect(self.addr)
-            return self.client.recv(2048).decode()
+            return pickle.loads(self.client.recv(2048))
         except:
             pass
 
     def send(self, data):
         try:
-            self.client.send(str.encode(data))
-            return self.client.recv(2048).decode()
+            self.client.send(pickle.dumps(data))
+            return pickle.loads(self.client.recv(2048))
         except socket.error as e:
             print(e)
-
-
-n = Network()
-print(n.send("Hello"))
-print(n.send("Como fue"))
--- server.py
+++ server.py
@@ -1,6 +1,9 @@
+import pickle
 import socket
 from _thread import *
 import sys
+
+from player import Player
 
 server = "192.168.0.242"
 port = 5555
@@ -15,30 +18,38 @@ except socket.error as e:
 s.listen(2)
 print("Waiting for connection, Server started")
 
-def threaded_client(conn):
-    conn.send(str.encode("Connected"))
+players = [Player(0, 0, 50, 50, (255, 0, 0)), Player(100, 100, 50, 50, (0, 0, 255))]
 
+def threaded_client(conn, player):
+    conn.send(pickle.dumps(players[player]))
     reply = ""
     while True:
         try:
-            data = conn.recv(2048)
-            reply = data.decode("utf-8")
+            data = pickle.loads(conn.recv(2048))
+            players[player] = data
+
             if not data:
                 print("Disconnected")
                 break
             else:
-                print("Received: ", reply)
+                if player == 1:
+                    reply = players[0]
+                else:
+                    reply = players[1]
+                print("Received: ", data)
                 print("Sending: ", reply)
 
-            conn.sendall(str.encode(reply))
+            conn.sendall(pickle.dumps(reply))
         except:
             break
 
     print("Lost connection")
     conn.close()
 
+current_player = 0
 
 while True:
     conn, addr = s.accept()
     print("Connected to: ", addr)
-    start_new_thread(threaded_client, (conn, ))
+    start_new_thread(threaded_client, (conn, current_player))
+    current_player += 1
